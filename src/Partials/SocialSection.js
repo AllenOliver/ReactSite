866d382b55bec8628e260443ed16cf69e5805f95
Allen Oliver <32452527+AllenOliver@users.noreply.github.com>
2019-03-15 16:13:14 -0400

    Added some styles and formatting

--- src/Partials/SocialSection.js
+++ src/Partials/SocialSection.js
@@ -4,16 +4,12 @@ class SocialSection extends React.Component {
     render(){
         return(
         <div className="socialContaner">
-            <div className="row" id="socialRow">
-            <ul className="socialList">
-                <li id="SocialIcon"><i class="fab fa-facebook"></i></li>
-                <li id="SocialIcon"><i class="fab fa-twitter"></i></li>
-                <li id="SocialIcon"><i class="fab fa-linkedin"></i></li>
-
-            </ul>
-                <div className="col-sm-4" id="SocialIcon"><i class="fab fa-facebook"></i></div>
-                <div className="col-sm-4" id="SocialIcon"><i class="fab fa-twitter"></i></div>
-                <div className="col-sm-4" id="SocialIcon"><i class="fab fa-linkedin"></i></div>
+            <div className="row justify-content-center" id="socialRow">
+                <ul className="socialList">
+                    <li id="SocialIcon"><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li id="SocialIcon"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li id="SocialIcon"><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                </ul>
             </div>
         </div>
         );
